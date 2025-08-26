d6ffa8d41650f05e424ad2fb0987824b3c209fc6
agananya03 <agananya54126@gmail.com>
2025-08-26 19:57:46 +0530

    week-22

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,26 @@
+import { WebSocketServer } from "ws"
+import jwt, { JwtPayload } from "jsonwebtoken"
+require('dotenv').config();
+const JWT_SECRET = process.env.JWT_SECRET as string;
+const wss = new WebSocketServer({port: 8080});
+wss.on('connection', function connection(ws, request) {
+    const url = request.url;
+    if(!url) {
+        return
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token');
+    const decoded = jwt.verify(token as string, JWT_SECRET);
+    if(typeof decoded == "string") {
+        ws.close();
+        return;
+    }
+    if(!decoded || !(decoded).userId) {
+        ws.close();
+        return
+    }
+    ws.on('message', function message(data) {
+        ws.send('pong')
+    });
+
+});
