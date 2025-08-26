873ac66879f2de01b19149c2efcb0391dcb81dbd
agananya03 <agananya54126@gmail.com>
2025-08-26 20:16:59 +0530

    week-22

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,27 +1,50 @@
 import express from "express"
 import jwt from "jsonwebtoken"
 import { middleware } from "./middleware";
-const JWT_SECRET = process.env.JWT_SECRET;
+import { JWT_SECRET } from "@repo/backend-common/config";
+import {CreateuserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types"
 
 const app = express()
 app.use(express.json());
 
 app.post("/signup", async(req, res) => {
-
+    const data = CreateuserSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+    res.json({
+        userId: "123"
+    })
 })
 
 app.post("/signin", async(req, res) => {
-    
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     const userId = 1
     const token = jwt.sign({
         userId
-    }, JWT_SECRET)
+    }, JWT_SECRET as string)
     res.json({
         token
     })
 })
 
 app.post("/room",middleware, async(req, res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     res.json({
         roomId: 123
     })
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken"
-require('dotenv').config();
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers["authorization"] ?? "";
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(token,JWT_SECRET as string);
     if(decoded) {
         //@ts-ignore
         req.userId = decoded.userId;
